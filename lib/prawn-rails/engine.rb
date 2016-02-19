4f30acc2501404f87287598330c59e051d910223
Shawn Balestracci <shawn@roshreview.com>
2016-02-18 23:03:33 -0500

    backport fix to remove mime constant redeclaration warning

--- lib/prawn-rails/engine.rb
+++ lib/prawn-rails/engine.rb
@@ -2,6 +2,8 @@ require "prawn-rails/prawn_handler"
 module PrawnRails
   class Engine < Rails::Engine
     ActionView::Template.register_template_handler(:prawn,PrawnRails::PrawnHandler)
-    Mime::Type.register "application/pdf", :pdf
+    if !Mime::Type.lookup_by_extension(:pdf)
+    	Mime::Type.register "application/pdf", :pdf
+    end
   end
 end
